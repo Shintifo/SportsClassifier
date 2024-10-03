9200b3b28f97d523a0ea1964b4348434389584c8
t <t.brayko@innopolis.university>
2024-10-03 21:35:41 +0300

    Minor changes in streamlit script

--- code/deployment/streamlit_app.py
+++ code/deployment/streamlit_app.py
@@ -1,13 +1,38 @@
 import streamlit as st
 import requests
+from PIL import Image
 
 
-st.title("Image Sports Classification")
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+def show_img(file):
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
 
-if uploaded_file is not None:
+
+def main():
+    st.title("Image Sports Classification")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        show_img(uploaded_file)
+        if st.button("Predict", use_container_width=True):
+            predict(uploaded_file)
+
+def predict(uploaded_file):
     files = {"file": uploaded_file.getvalue()}
     response = requests.post("http://localhost:8000/predict/", files=files)
     prediction = response.json()["prediction"]
-    st.write(f"Prediction: {prediction}")
+
+    output = f"""
+        <div style="text-align: center; font-size: 24px;">
+            Prediction: <b>{prediction}</b>
+        </div>
+    """
+
+    st.markdown(output, unsafe_allow_html=True)
+    # st.write(f"Prediction: {prediction}")
+
+
+if __name__ == '__main__':
+    main()
